f7f2babc1cf63871e4e1b7932346034cabe06164
GameFuzzy <mail@gamefuzzy.dev>
2025-04-28 16:22:58 +0200

    Fix /delete with non-existing ID not returning 404

--- src/users/api.py
+++ src/users/api.py
@@ -1,11 +1,13 @@
 from django.contrib.gis.geos.point import Point
 from django.contrib.gis.geos.polygon import Polygon
+from django.http import Http404
 from geojson_pydantic import Point as PointType
 from ninja import ModelSchema, Router, Schema
 
 from .models import User
 
 router = Router()
+
 
 # Schema for returning a user
 class UserOut(ModelSchema):
@@ -15,38 +17,48 @@ class UserOut(ModelSchema):
         model = User
         model_fields = ["id"]
 
+
 # Schema for creating a user
 class UserIn(Schema):
-    longitude: float # Longitude
-    latitude: float # Latitude
+    longitude: float
+    latitude: float
+
 
 # Endpoint for creating a user
 @router.post("/create")
 async def create_user(request, data: UserIn):
-    user = await User.objects.acreate(geo_pos=Point(data.longitude,
-                                                    data.latitude))
+    user = await User.objects.acreate(geo_pos=Point(data.longitude, data.latitude))
     return {"id": user.id}
-    
+
+
 # Endpoint for deleting a user
 @router.delete("/delete")
 async def delete_user(request, user_id: int):
-    await User.objects.filter(id=user_id).adelete()
+    try:
+        user = await User.objects.aget(pk=user_id)
+        await user.adelete()
+    except:
+        raise Http404
     return {"success": True}
+
 
 # Endpoint for viewing all users
 @router.get("/view-all", response=list[UserOut])
 async def view_all_users(request):
     return [user async for user in User.objects.all()]
 
+
 # Endpoint for viewing all users within a given bounding box
 @router.get("/view-within-bbox", response=list[UserOut])
-async def view_users_within_bounding_box(request, longitude_min: float,
-                                         latitude_min: float,
-                                         longitude_max: float, latitude_max:
-                                         float):
+async def view_users_within_bounding_box(
+    request,
+    longitude_min: float,
+    latitude_min: float,
+    longitude_max: float,
+    latitude_max: float,
+):
 
-    bbox = (longitude_min, latitude_min,
-                                         longitude_max, latitude_max)
+    bbox = (longitude_min, latitude_min, longitude_max, latitude_max)
     geom = Polygon.from_bbox(bbox)
 
     return [user async for user in User.objects.filter(geo_pos__within=geom)]
